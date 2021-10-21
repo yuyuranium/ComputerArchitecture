f1508049a2a3a121294a88ec2b08f5dcb17e339d
yuyuranium <e24076459@gs.ncku.edu.tw>
2021-10-21 16:05:31 +0800

    Update assembly code for optimization

--- hw1/twoSum.s
+++ hw1/twoSum.s
@@ -1,20 +1,21 @@
 .data
-size:   .word 8
 num:    .word 2, 3, 5, 7, 11, 13, 17, 19
-target: .word 18
+target: .word 12
 str1:   .string "numbers = "
 str2:   .string "\ntarget = "
 str3:   .string "sol = "
 lf:     .string "\n"
 ws:     .string " "
-sol:    .dword 0
+sol:    .word 0, 0
 
 .text
 main:
-        addi    sp, sp, -12
-        sw      ra, 8(sp)       # save sp
-        la      t0, num
-        sw      t0, 4(sp)
+        addi    sp, sp, -16
+        sw      ra, 12(sp)      # save ra
+        sw      s1, 8(sp)       # num
+        sw      s0, 4(sp)       # i
+
+        la      s0, num
 
         # print "numbers = "
         la      a0, str1
@@ -22,15 +23,14 @@ main:
         ecall
 
         # First for loop
-        li      t1, 0           # i = 0
-        li      t4, 8
+        li      s1, 0           # i = 0
+        li      a1, 8           # size = 8
+        j       L_for0C
 L_for0:
-        bge     t1, t4, L_for0E
-
         # Calculate the address of numbers[i] and print the value
-        slli    t5, t1, 2
-        add     t2, t0, t5
-        lw      a0, 0(t2)
+        slli    t1, s1, 2
+        add     t1, s0, t1
+        lw      a0, 0(t1)
         li      a7, 1
         ecall
 
@@ -39,11 +39,10 @@ L_for0:
         li      a7, 4
         ecall
 
-        # ++i
-        addi    t1, t1, 1
-        j       L_for0
+        addi    s1, s1, 1       # ++i
+L_for0C:
+        blt     s1, a1, L_for0
 L_for0E:
-        addi    x0, x0, 0
 
         # Print "target = "
         la      a0, str2
@@ -61,9 +60,9 @@ L_for0E:
         li      a7, 4
         ecall
 
-        lw      a0, 4(sp)
-        li      a1, 8
-        addi    a3, sp, 0
+        # Call subroutine twoSum
+        mv      a0, s0          # num
+        addi    a3, sp, 0       # &returnSize = 0(sp)
         jal     ra, twoSum
         mv      s0, a0          # sol
 
@@ -73,15 +72,14 @@ L_for0E:
         ecall
 
         # Second for loop
-        li      t0, 0
+        li      s1, 0           # i = 0
         lw      t1, 0(sp)
+        j       L_for1
 L_for1:
-        bge     t0, t1, L_for1E
-
         # Calculate the address of sol[i] and print the value
-        slli    t2, t0, 2
+        slli    t2, s1, 2
         add     t2, s0, t2
-        lw      a0, 0(t2)  # sol[i]
+        lw      a0, 0(t2)       # sol[i]
         li      a7, 1
         ecall
 
@@ -90,17 +88,19 @@ L_for1:
         li      a7, 4
         ecall
 
-        # ++i
-        addi    t0, t0, 1
-        j       L_for1
+        addi    s1, s1, 1       # ++i
+L_for1C:
+        blt     s1, t1, L_for1
 L_for1E:
         # Print "\n"
         la      a0, lf
         li      a7, 4
         ecall
         
-        lw      ra, 8(sp)
-        addi    sp, sp, 12
+        lw      ra, 12(sp)      # save ra
+        lw      s1, 8(sp)       # num
+        lw      s0, 4(sp)       # i
+        addi    sp, sp, 16
 
         # Exit program
         li a7, 10
@@ -111,8 +111,10 @@ L_for1E:
 # a2: target
 # a3: &returnSize
 twoSum:
-        addi    sp, sp, -4
-        sw      ra, 0(sp)
+        addi    sp, sp, -12
+        sw      s0, 0(sp)
+        sw      s1, 4(sp)
+        sw      ra, 8(sp)
 
         # p = 0, r = numbersSize - 1
         addi    s0, zero, 0
@@ -124,24 +126,28 @@ L_while:
         add     t1, a0, t1
         lw      t2, 0(t0)
         lw      t3, 0(t1)
-        add     t4, t2, t3
+        add     t4, t2, t3      # sum = numbers[p] + numbers[r]
         bge     t4, a2, L_else0
-        addi    s0, s0, 1
+        addi    s0, s0, 1       # ++p
         j       L_if0E
 L_else0:
         bge     a2, t4, L_else1
-        addi    s1, s1, -1
+        addi    s1, s1, -1      # --r
         j       L_if0E
 L_else1:
-        la      a0, sol
-        addi    t0, s0, 1
-        addi    t1, s1, 1
-        sw      t0, 0(a0)
-        sw      t1, 4(a0)
+        la      a0, sol         # fake malloc
+        addi    t0, s0, 1       # p + 1
+        addi    t1, s1, 1       # r + 1
+        sw      t0, 0(a0)       # sol[0] = p + 1
+        sw      t1, 4(a0)       # sol[1] = r + 1
         li      t0, 2
-        sw      t0, 0(a3)
-        lw      ra, 0(sp)
-        addi    sp, sp, 4
+        sw      t0, 0(a3)       # *returnSize = 2
+
+        # Returning from twoSum
+        lw      s0, 0(sp)
+        lw      s1, 4(sp)
+        lw      ra, 8(sp)
+        addi    sp, sp, 12
         jr      ra
 L_if0E:
         j       L_while
